0ebe9bbb7c32f63c3cc96b06bd1981c754bea8be
Robert Radulescu <robertdanielradulescu@gmail.com>
2026-06-09 12:52:55 +0200

    feat: add postgres with docker compose and flyway migrations

--- src/test/java/com/robertradulescu/lingo/LingoApplicationTests.java
+++ src/test/java/com/robertradulescu/lingo/LingoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LingoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
